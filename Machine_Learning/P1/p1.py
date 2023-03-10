735f88e7685eb1bcb135fc38050fd9b29cb38090
Shiska07 <rautshiska@gmail.com>
2023-03-10 13:55:04 -0600

    implement softmax regression

--- Machine_Learning/P1/p1.py
+++ Machine_Learning/P1/p1.py
@@ -1,8 +1,7 @@
 import numpy as np
 import sys
 
-# given a file
-def get_X_Y_arrays(filename):
+def get_X_Y_arrays(filename, dtype_x, dtype_y):
     try:
         f = open(filename, 'r')
     except OSError:
@@ -32,14 +31,14 @@ def get_X_Y_arrays(filename):
             line = f.readline()
         
         # create dataframe of features and append labels
-        X = np.array(features, dtype = float, ndmin = 2)
+        X = np.array(features, dtype = dtype_x, ndmin = 2)
         
         # convert labels list to array
-        Y = np.array(labels, dtype = str, ndmin = 2)
+        Y = np.array(labels, dtype = dtype_y, ndmin = 2)
         
         return X.transpose(), Y
     
-def get_X_array(filename):
+def get_X_array(filename, dtype_x):
     try:
         f = open(filename, 'r')
     except OSError:
@@ -63,10 +62,25 @@ def get_X_array(filename):
             line = f.readline()
         
         # create dataframe of features and append labels
-        X = np.array(features, dtype = float, ndmin = 2)
+        X = np.array(features, dtype = dtype_x, ndmin = 2)
         
         return X.transpose()
+
+
+
+# compares two arrays and returns accuracy
+def get_acc(Y_pred, Y):
+    n_samples = Y_pred.shape[1]
+    return (np.sum(Y_eval == Y_pred))/n_samples
+
+# adds bias as the first row to a dataset
+def add_bias(X):
     
+    n_feat, n_samples = X.shape
+    X_b = np.ones((n_feat+1, n_samples), dtype = float)
+    X_b[1::,:] = X
+    
+    return X_b
 
 # given an array of attribute values for a categocial attribute,
 # preforms one-hot-encoding and returns resulting binary array
@@ -108,11 +122,7 @@ def get_sample_prediction_label(sfmax_net, label_idx_dict):
 
 # uses softmax function and parameter matrix to get probability values
 # for multiclass classification
-def get_sample_prediction_values(x, model_params):
-    
-    # initialize column vector of ones to add bias
-    x_sample = np.ones((x.shape[0]+1, 1), dtype = float)
-    x_sample[1::] = x.reshape(x.shape[0], 1)
+def get_sample_prediction_values(x_sample, model_params):
     
     # calculate linear net value
     net = np.dot(model_params, x_sample)
@@ -132,11 +142,11 @@ def get_predictions(X_test, model_params, label_idx_dict):
     Y_pred = []
 
     # get number of test samples
-    __, n_samples = X_test.shape
-
+    n_dim, n_samples = X_test.shape
 
     for i in range(n_samples):
-
+    
+        x_sample = X_test[:,i].rechape(n_dim, 1)
         y_pred_values = get_sample_prediction_values(X_test[:,i], model_params)
         y_pred_label = get_sample_prediction_label(y_pred_values, label_idx_dict)
         Y_pred.append(y_pred_label)
@@ -144,8 +154,7 @@ def get_predictions(X_test, model_params, label_idx_dict):
     # convert labels list to numpy array
     Y_pred = np.array(Y_pred, dtype = str, ndmin = 2)
 
-    return Y_pred  
-
+    return Y_pred 
 
 # trains a softmax regression model given training data, alpha and number of epochs
 def train_softmax_regressor(X_train, Y_train, alpha, epochs):
@@ -155,59 +164,69 @@ def train_softmax_regressor(X_train, Y_train, alpha, epochs):
     
     # get no of classes/labels
     n_class, __ = Y_train.shape
-    
+
     # get paramater matrix
-    model_params = np.random.uniform(-1, 1, size = (n_class, n_feat+1))
+    model_params = np.random.uniform(0, 0.01, size = (n_class, n_feat))
+
+    # initialize list to store net change in parameter values
+    change_model_params = []
     
     for i in range(epochs):
 
         # initialize gradient vector for each epoch
-        gradient_mtx = np.zeros((n_class, n_feat+1))
+        gradient_mtx = np.zeros((n_class, n_feat), dtype = float)
 
         for j in range(n_samples):
             
-            # pick a sample randomly
-            idx = np.random.randint(0, n_samples)
-            x_sample = X_train[:,idx]
-            y_sample = Y_train[:,idx].reshape(Y_train.shape[0], 1)
+            # pick a sample 
+            x_sample = X_train[:,j].reshape(n_feat,1)
+            y_sample = Y_train[:,j].reshape(n_class, 1)
             
-            # get prediction value and adjust weights
-            # bias will be added to the sample in the function 
+            # get prediction value
             y_pred = get_sample_prediction_values(x_sample, model_params)
 
-            # add bias to x_sample before calculating gradient
-            x_sample_b = np.ones((x_sample.shape[0]+1, 1), dtype = float)
-            x_sample_b[1::] = x_sample.reshape(x_sample.shape[0], 1)
-
             # calculate gradient matrix
-            sample_gradient = np.dot((y_sample - y_pred), x_sample_b.transpose())
+            sample_gradient = np.dot((y_sample - y_pred), x_sample.transpose())
             gradient_mtx = gradient_mtx + sample_gradient
             
-        # adjust parameter values using batch gradient ascent
-        model_params = model_params + alpha*gradient_mtx
+        # adjust parameter values using batch gradient descent 
+        updated_params = model_params + (alpha*gradient_mtx)
+        
+        # get the net change in parameters
+        net_change = np.sum(np.abs(model_params - updated_params))
+        change_model_params.append(net_change)
+            
+        # set updated parameters as new parameters  
+        model_params = updated_params.copy()
         
     # return final parameter matrix
     return model_params
 
+
 def leave_one_out_evaluation(X_eval, Y_eval, alpha, epochs):
-    
+
+    # add bias to X
+    X_train_b = add_bias(X_eval)
+
     # get number of features and samples
     n_feat, n_samples = X_eval.shape
-    
+
     # prediction labels generated by 'predict_class_with_knn' will be stored in this list
     Y_pred = []
 
     # get encoded array for y
     Y_train_encoded, label_idx_dict = one_hot_encoder(Y_eval)
+    n_class, __ = Y_train_encoded.shape
     
+    # trains a new model to predict each sample
     for i in range(n_samples):
         
         # pick test datapoint
-        x_test = X_eval[:,i]
-        y_test = Y_train_encoded[:,i]
+        x_test = X_train_b[:,i].reshape(n_feat, 1)
+        y_test = Y_train_encoded[:,i].reshape(n_class, 1)
         
         # create traiing set by deleting test datapoint
-        X_train = np.delete(X_eval, i, axis = 1)
+        X_train = np.delete(X_train_b, i, axis = 1)
         Y_train = np.delete(Y_train_encoded, i, axis = 1)
         
         # train model
@@ -220,19 +239,19 @@ def leave_one_out_evaluation(X_eval, Y_eval, alpha, epochs):
     
     # convert prediction list to numpy array
     Y_pred = np.array(Y_pred, dtype = str, ndmin = 2)
-    acc = (np.sum(Y_eval == Y_pred))/n_samples
+    acc = get_acc(Y_pred, Y_eval)
     
     # return predictions and accuracy
     return Y_pred, acc
 
 fname_eval = 'Data/3_eval.txt'
 
-X_eval, Y_eval = get_X_Y_arrays(fname_eval)
+X_eval, Y_eval = get_X_Y_arrays(fname_eval, float, str)
 
-X_eval = np.delete(X_eval, 3, axis = 0)
+# X_eval = np.delete(X_eval, 3, axis = 0)
 # model_params, label_idx_dict = train_softmax_regressor(X_train, Y_train, 0.0001, 1)
-Y_pred, acc = leave_one_out_evaluation(X_eval, Y_eval, 0.001, 100)
+Y_pred, acc = leave_one_out_evaluation(X_eval, Y_eval, 0.001, 1000)
 
-
+# 0.001, 1000
 
 
